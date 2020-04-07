f2f3f5134e3e764fecf9c01a18e19ed698927164
Ash Davies <ashdavies@users.noreply.github.com>
2020-04-07 17:46:48 +0200

    Update deprecation messages

--- src/main/kotlin/io/ashdavies/extensions/SharedPreferencesStore.kt
+++ src/main/kotlin/io/ashdavies/extensions/SharedPreferencesStore.kt
@@ -2,13 +2,12 @@ package io.ashdavies.extensions
 
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
-import io.ashdavies.preferences.SharedPreferencesStore
 import io.ashdavies.properties.DefaultSharedPreferencesProperty
 import io.ashdavies.properties.LegacySharedPreferencesValue
 import io.ashdavies.properties.NullableSharedPreferencesProperty
 import io.ashdavies.properties.SharedPreferencesProperty
 
-fun SharedPreferencesStore.booleanOrDefault(default: Boolean): SharedPreferencesProperty<Boolean> = DefaultSharedPreferencesProperty(
+fun booleanOrDefault(default: Boolean): SharedPreferencesProperty<Boolean> = DefaultSharedPreferencesProperty(
         getValue = SharedPreferences::getBoolean,
         setValue = Editor::putBoolean,
         default = default
@@ -17,17 +16,17 @@ fun SharedPreferencesStore.booleanOrDefault(default: Boolean): SharedPreferences
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesBooleanOrDefault(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesBooleanOrDefault"]
+                expression = "booleanOrDefault(default)",
+                imports = ["io.ashdavies.extensions.booleanOrDefault"]
         )
 )
-fun SharedPreferencesStore.booleanOrDefault(key: String, default: Boolean): SharedPreferencesProperty<Boolean> = DefaultSharedPreferencesProperty(
+fun booleanOrDefault(key: String, default: Boolean): SharedPreferencesProperty<Boolean> = DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getBoolean, Editor::putBoolean, key),
         setValue = Editor::putBoolean,
         default = default
 )
 
-fun SharedPreferencesStore.floatOrDefault(default: Float): SharedPreferencesProperty<Float> = DefaultSharedPreferencesProperty(
+fun floatOrDefault(default: Float): SharedPreferencesProperty<Float> = DefaultSharedPreferencesProperty(
         getValue = SharedPreferences::getFloat,
         setValue = Editor::putFloat,
         default = default
@@ -36,17 +35,17 @@ fun SharedPreferencesStore.floatOrDefault(default: Float): SharedPreferencesProp
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesFloatOrDefault(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesFloatOrDefault"]
+                expression = "floatOrDefault(default)",
+                imports = ["io.ashdavies.extensions.floatOrDefault"]
         )
 )
-fun SharedPreferencesStore.floatOrDefault(key: String, default: Float): SharedPreferencesProperty<Float> = DefaultSharedPreferencesProperty(
+fun floatOrDefault(key: String, default: Float): SharedPreferencesProperty<Float> = DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getFloat, Editor::putFloat, key),
         setValue = Editor::putFloat,
         default = default
 )
 
-fun SharedPreferencesStore.intOrDefault(default: Int): SharedPreferencesProperty<Int> = DefaultSharedPreferencesProperty(
+fun intOrDefault(default: Int): SharedPreferencesProperty<Int> = DefaultSharedPreferencesProperty(
         getValue = SharedPreferences::getInt,
         setValue = Editor::putInt,
         default = default
@@ -55,17 +54,17 @@ fun SharedPreferencesStore.intOrDefault(default: Int): SharedPreferencesProperty
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesIntOrDefault(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesIntOrDefault"]
+                expression = "intOrDefault(default)",
+                imports = ["io.ashdavies.extensions.intOrDefault"]
         )
 )
-fun SharedPreferencesStore.intOrDefault(key: String, default: Int): SharedPreferencesProperty<Int> = DefaultSharedPreferencesProperty(
+fun intOrDefault(key: String, default: Int): SharedPreferencesProperty<Int> = DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getInt, Editor::putInt, key),
         setValue = Editor::putInt,
         default = default
 )
 
-fun SharedPreferencesStore.longOrDefault(default: Long): SharedPreferencesProperty<Long> = DefaultSharedPreferencesProperty(
+fun longOrDefault(default: Long): SharedPreferencesProperty<Long> = DefaultSharedPreferencesProperty(
         getValue = SharedPreferences::getLong,
         setValue = Editor::putLong,
         default = default
@@ -74,17 +73,17 @@ fun SharedPreferencesStore.longOrDefault(default: Long): SharedPreferencesProper
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesLongOrDefault(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesLongOrDefault"]
+                expression = "longOrDefault(default)",
+                imports = ["io.ashdavies.extensions.longOrDefault"]
         )
 )
-fun SharedPreferencesStore.longOrDefault(key: String, default: Long): SharedPreferencesProperty<Long> = DefaultSharedPreferencesProperty(
+fun longOrDefault(key: String, default: Long): SharedPreferencesProperty<Long> = DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getLong, Editor::putLong, key),
         setValue = Editor::putLong,
         default = default
 )
 
-fun SharedPreferencesStore.string(): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
+fun string(): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
         getValue = SharedPreferences::getString,
         setValue = Editor::putString,
         block = { requireNotNull(it) }
@@ -93,17 +92,17 @@ fun SharedPreferencesStore.string(): SharedPreferencesProperty<String> = Nullabl
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesString(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesString"]
+                expression = "string(default)",
+                imports = ["io.ashdavies.extensions.string"]
         )
 )
-fun SharedPreferencesStore.string(key: String): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
+fun string(key: String): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getString, Editor::putString, key),
         setValue = Editor::putString,
         block = { requireNotNull(it) }
 )
 
-fun SharedPreferencesStore.stringOrDefault(default: String): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
+fun stringOrDefault(default: String): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
         getValue = SharedPreferences::getString,
         setValue = Editor::putString,
         block = { it ?: default }
@@ -112,17 +111,17 @@ fun SharedPreferencesStore.stringOrDefault(default: String): SharedPreferencesPr
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesStringOrDefault(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesStringOrDefault"]
+                expression = "stringOrDefault(default)",
+                imports = ["io.ashdavies.extensions.stringOrDefault"]
         )
 )
-fun SharedPreferencesStore.stringOrDefault(key: String, default: String): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
+fun stringOrDefault(key: String, default: String): SharedPreferencesProperty<String> = NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getString, Editor::putString, key),
         setValue = Editor::putString,
         block = { it ?: default }
 )
 
-fun SharedPreferencesStore.stringOrNull(): SharedPreferencesProperty<String?> = NullableSharedPreferencesProperty(
+fun stringOrNull(): SharedPreferencesProperty<String?> = NullableSharedPreferencesProperty(
         getValue = SharedPreferences::getString,
         setValue = Editor::putString,
         block = { it }
@@ -131,17 +130,17 @@ fun SharedPreferencesStore.stringOrNull(): SharedPreferencesProperty<String?> = 
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesStringOrNull(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesStringOrNull"]
+                expression = "stringOrNull()",
+                imports = ["io.ashdavies.extensions.stringOrNull"]
         )
 )
-fun SharedPreferencesStore.stringOrNull(key: String): SharedPreferencesProperty<String?> = NullableSharedPreferencesProperty(
+fun stringOrNull(key: String): SharedPreferencesProperty<String?> = NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getString, Editor::putString, key),
         setValue = Editor::putString,
         block = { it }
 )
 
-fun SharedPreferencesStore.stringSetOrDefault(default: Set<String>): SharedPreferencesProperty<Set<String>> = NullableSharedPreferencesProperty(
+fun stringSetOrDefault(default: Set<String>): SharedPreferencesProperty<Set<String>> = NullableSharedPreferencesProperty(
         getValue = SharedPreferences::getStringSet,
         setValue = Editor::putStringSet,
         block = { it ?: default }
@@ -150,13 +149,12 @@ fun SharedPreferencesStore.stringSetOrDefault(default: Set<String>): SharedPrefe
 @Deprecated(
         message = "Key is deprecated",
         replaceWith = ReplaceWith(
-                expression = "sharedPreferencesStringSetOrDefault(default)",
-                imports = ["de.is24.mobile.extensions.sharedPreferencesStringSetOrDefault"]
+                expression = "stringSetOrDefault(default)",
+                imports = ["io.ashdavies.extensions.stringSetOrDefault"]
         )
 )
-fun SharedPreferencesStore.stringSetOrDefault(key: String, default: Set<String>): SharedPreferencesProperty<Set<String>> = NullableSharedPreferencesProperty(
+fun stringSetOrDefault(key: String, default: Set<String>): SharedPreferencesProperty<Set<String>> = NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(SharedPreferences::getStringSet, Editor::putStringSet, key),
         setValue = Editor::putStringSet,
         block = { it ?: default }
 )
-
