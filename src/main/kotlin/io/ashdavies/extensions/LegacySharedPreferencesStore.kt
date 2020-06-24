f6abb9cd8ca2e659fdcfcaaf7c87dc11f2a08b47
Ash Davies <ashdavies@users.noreply.github.com>
2020-06-24 20:27:04 +0200

    Improve documentation for deprecated functions

--- src/main/kotlin/io/ashdavies/extensions/LegacySharedPreferencesStore.kt
+++ src/main/kotlin/io/ashdavies/extensions/LegacySharedPreferencesStore.kt
@@ -8,153 +8,137 @@ import io.ashdavies.properties.NullableSharedPreferencesProperty
 import io.ashdavies.properties.SharedPreferencesProperty
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.booleanOrDefault(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.booleanOrDefault(default)")
 )
-fun booleanOrDefault(key: String, default: Boolean): SharedPreferencesProperty<Boolean> =
+fun booleanOrDefault(oldName: String, default: Boolean): SharedPreferencesProperty<Boolean> =
     DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getBoolean,
             setValue = Editor::putBoolean,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putBoolean,
         default = default
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.floatOrDefault(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.floatOrDefault(default)")
 )
-fun floatOrDefault(key: String, default: Float): SharedPreferencesProperty<Float> =
+fun floatOrDefault(oldName: String, default: Float): SharedPreferencesProperty<Float> =
     DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getFloat,
             setValue = Editor::putFloat,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putFloat,
         default = default
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.intOrDefault(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.intOrDefault(default)")
 )
-fun intOrDefault(key: String, default: Int): SharedPreferencesProperty<Int> =
+fun intOrDefault(oldName: String, default: Int): SharedPreferencesProperty<Int> =
     DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getInt,
             setValue = Editor::putInt,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putInt,
         default = default
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.longOrDefault(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.longOrDefault(default)")
 )
-fun longOrDefault(key: String, default: Long): SharedPreferencesProperty<Long> =
+fun longOrDefault(oldName: String, default: Long): SharedPreferencesProperty<Long> =
     DefaultSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getLong,
             setValue = Editor::putLong,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putLong,
         default = default
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.string(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.string(default)")
 )
-fun string(key: String): SharedPreferencesProperty<String> =
+fun string(oldName: String): SharedPreferencesProperty<String> =
     NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getString,
             setValue = Editor::putString,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putString,
         block = { requireNotNull(it) }
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.stringOrDefault(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.stringOrDefault(default)")
 )
-fun stringOrDefault(key: String, default: String): SharedPreferencesProperty<String> =
+fun stringOrDefault(oldName: String, default: String): SharedPreferencesProperty<String> =
     NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getString,
             setValue = Editor::putString,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putString,
         block = { it ?: default }
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.stringOrNull()"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.stringOrNull()")
 )
-fun stringOrNull(key: String): SharedPreferencesProperty<String?> =
+fun stringOrNull(oldName: String): SharedPreferencesProperty<String?> =
     NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getString,
             setValue = Editor::putString,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putString,
         block = { it }
     )
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.stringSetOrDefault(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.stringSetOrDefault(default)")
 )
-fun stringSetOrDefault(key: String, default: Set<String>): SharedPreferencesProperty<Set<String>> =
-    NullableSharedPreferencesProperty(
-        getValue = LegacySharedPreferencesValue(
-            getValue = SharedPreferences::getStringSet,
-            setValue = Editor::putStringSet,
-            oldName = key
-        ),
+fun stringSetOrDefault(
+    oldName: String,
+    default: Set<String>
+): SharedPreferencesProperty<Set<String>> = NullableSharedPreferencesProperty(
+    getValue = LegacySharedPreferencesValue(
+        getValue = SharedPreferences::getStringSet,
         setValue = Editor::putStringSet,
-        block = { it ?: default }
-    )
+        oldName = oldName
+    ),
+    setValue = Editor::putStringSet,
+    block = { it ?: default }
+)
 
 @Deprecated(
-    message = "Key is deprecated",
-    replaceWith = ReplaceWith(
-        expression = "io.ashdavies.extensions.stringSetOrNull(default)"
-    )
+    message = "Explicitly named preference delegate properties are deprecated",
+    replaceWith = ReplaceWith("io.ashdavies.extensions.stringSetOrNull(default)")
 )
-fun stringSetOrNull(key: String): SharedPreferencesProperty<Set<String>?> =
+fun stringSetOrNull(oldName: String): SharedPreferencesProperty<Set<String>?> =
     NullableSharedPreferencesProperty(
         getValue = LegacySharedPreferencesValue(
             getValue = SharedPreferences::getStringSet,
             setValue = Editor::putStringSet,
-            oldName = key
+            oldName = oldName
         ),
         setValue = Editor::putStringSet,
         block = { it }
